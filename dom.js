e87fe2d9f56a7a006ce9622568cc86860f66f896
Sizan-Ahmed <asizan631@gmail.com>
2021-08-05 17:38:35 +0600

    normal change

--- dom.js
+++ dom.js
@@ -1,2 +1,33 @@
-const select = document.querySelector(".para")
-console.log(select)
+// practice innerText and InnerHTML
+// let inText = document.getElementById("main").innerText;
+// console.log(inText)
+
+// let inHtm = document.getElementById("main").innerHTML;
+// console.log(inHtm)
+
+
+// let abc = document.getElementById("para").innerHTML = "<h1> hi </h1>";
+// console.log(abc)
+
+// // practice setAttribute
+// let efg = document.getElementById("para").setAttribute("class", "xyx");
+// // practice getAttribute
+// efg = document.getElementById("para").getAttribute("class")
+//     // practice Attribute
+// efg = document.getElementById("para").attributes[0]
+// console.log(efg);
+
+// // practice querySelector
+// const select = document.getElementsByTagName("p");
+// console.log(select);
+
+// practice style method className classList;
+
+// document.querySelector("#red").classList.remove("xyz");
+// let abc = document.querySelector("#red").classList;
+// console.log(abc)
+
+// practice addEventListener
+const abc = document.querySelector("#red").addEventListener("click", function() {
+    document.querySelector("#red").style.background = "blue";
+})
